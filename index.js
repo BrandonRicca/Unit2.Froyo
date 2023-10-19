e2819522c2e14af4bc10461cd36d70713895c4c1
BrandonRicca <tulshibrandon@gmail.com>
2023-10-19 14:13:20 -0400

    added objects

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 let iceCream = window.prompt("What's your favorite ice cream flavor?", "Vanilla, Strawberry, Coffee");
 let array = iceCream.split(",")
 console.table(array);
+
+let menu = {
+  // key : value
+  availableFlavors: ["Vanilla, Strawberry, Coffee, Chocolate, Coconut, Cherry"],
+  sizes: ["Small, Medium, Large"],
+  prices: ["Small = $3, Medium = $5, Large = $7"],
+  onSale: false
+};
+console.table(menu);
+
+console.log("--------------------------------------------------------------------------------------------------------------");
+console.log("We have a new flavor!!!");
+menu.availableFlavors.splice(4, 0, "Banana");
+console.table(menu);
